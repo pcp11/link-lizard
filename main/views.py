d177622e40164555ade0e113c5ea74539cf5d7d3
Patrick Christ <patrick.christ.91@web.de>
2020-06-13 21:53:00 +0200

    Get base url form request

--- main/views.py
+++ main/views.py
@@ -15,7 +15,9 @@ def success(request):
     url_param = request.POST.get("url")
     mapping = URLMapping(original_url=url_param)
     mapping.save()
-    generated_url = 'http://127.0.0.1:8000/' + mapping.generated_hash
+
+    base_url = request.META['HTTP_ORIGIN'] + "/"
+    generated_url = base_url + mapping.generated_hash
     return render(request, 'success.html', {'original_url': url_param, 'generated_url': generated_url})
 
 
