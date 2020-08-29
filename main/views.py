e7670fb526b726d02452985308a5da2f069eddd6
Patrick Christ <patrick.christ.91@web.de>
2020-08-29 23:41:20 +0200

    Refactor views

--- main/views.py
+++ main/views.py
@@ -1,28 +1,36 @@
-from django.http import HttpResponseRedirect
-from django.shortcuts import render
+import json
 
-from main.base64 import decode
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render, get_object_or_404
+from nanoid import generate
+
+from main.forms import URLMappingForm
 from main.models import URLMapping
 
 
 def index(request):
-    return render(request, 'home.html')
+    if request.method == 'POST':
+        form = URLMappingForm(request.POST)
+        if form.is_valid():
+            original_url = form.cleaned_data['original_url']
+            generated_hash = form.cleaned_data['generated_hash'] \
+                if form.cleaned_data['generated_hash'] \
+                else generate(size=5)
 
+            mapping = URLMapping(original_url=original_url, generated_hash=generated_hash)
+            mapping.save()
 
-def success(request):
-    if request.method != 'POST':
-        return render(request, 'home.html')
-    url_param = request.POST.get("url")
-    mapping = URLMapping(original_url=url_param)
-    mapping.save()
+            base_url = request.META['HTTP_ORIGIN'] + "/"
+            generated_url = base_url + mapping.generated_hash
 
-    base_url = request.META['HTTP_ORIGIN'] + "/"
-    generated_url = base_url + mapping.generated_hash
-    return render(request, 'success.html', {'original_url': url_param, 'generated_url': generated_url})
+            return HttpResponse(json.dumps({"original_url": original_url, "generated_url": generated_url}))
+        else:
+            return HttpResponse(json.dumps({"errors": form.errors}))
+    else:
+        form = URLMappingForm()
+    return render(request, 'home.html', {"form": form})
 
 
 def redirect(_, generated_hash):
-    mapping_id = int(decode(generated_hash))
-    mapping = URLMapping.objects.get(pk=mapping_id)
-
+    mapping = get_object_or_404(URLMapping, generated_hash=generated_hash)
     return HttpResponseRedirect(mapping.original_url)
