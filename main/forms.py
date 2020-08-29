94927d9893a4ad05b2a922de702ab5d62c084eeb
Patrick Christ <patrick.christ.91@web.de>
2020-08-29 23:40:37 +0200

    Remove validator

--- main/forms.py
+++ main/forms.py
@@ -1,11 +1,10 @@
-from django.core.validators import URLValidator
 from django.forms import ModelForm, TextInput, CharField
 
 from main.models import URLMapping
 
 
 class URLMappingForm(ModelForm):
-    original_url = CharField(validators=[URLValidator()], widget=TextInput(
+    original_url = CharField(widget=TextInput(
         attrs={"id": "original_url", "class": "form-control", "placeholder": "Enter your link"}))
     generated_hash = CharField(required=False, widget=TextInput(
         attrs={"id": "generated_hash", "class": "form-control", "placeholder": "Custom Alias (Optional)"}))
