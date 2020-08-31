2298f7dc23d4eebba338cf68e2b58c2be0dc72f9
Patrick Christ <patrick.christ.91@web.de>
2020-08-31 19:55:57 +0200

    Use WebDriverManager

--- main/tests.py
+++ main/tests.py
@@ -1,13 +1,17 @@
 from django.test import LiveServerTestCase, override_settings
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from webdriver_manager.firefox import GeckoDriverManager
+
+ORIGINAL_URL_TEST = 'https://www.djangoproject.com/'
+GENERATED_HASH_TEST = 'XXX'
 
 
 @override_settings(DEBUG=True)
 class FormTest(LiveServerTestCase):
 
     def setUp(self):
-        self.browser = webdriver.Firefox()
+        self.browser = webdriver.Firefox(executable_path=GeckoDriverManager().install())
         self.browser.implicitly_wait(10)  # seconds
         super(FormTest, self).setUp()
 
@@ -23,11 +27,10 @@ class FormTest(LiveServerTestCase):
         generated_hash_input = browser.find_element_by_id('generated_hash')
         submit_button = browser.find_element_by_css_selector('#form button')
 
-        original_url_input.send_keys('https://www.djangoproject.com/')
-        generated_hash_input.send_keys('XXX')
+        original_url_input.send_keys(ORIGINAL_URL_TEST)
+        generated_hash_input.send_keys(GENERATED_HASH_TEST)
         submit_button.send_keys(Keys.RETURN)
 
         generated_url_element = browser.find_element_by_css_selector('#generated_urls>.card a')
 
-        assert generated_url_element.text == '%s%s' % (self.live_server_url, '/XXX')
-
+        assert generated_url_element.text == '%s%s' % (self.live_server_url, '/' + GENERATED_HASH_TEST)
