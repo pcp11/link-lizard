a4303d2718eda61d4b9d789157eecabbd7f887c0
Patrick Christ <patrick.christ.91@web.de>
2020-08-31 20:23:31 +0200

    Deactivate Heroku TestRunner

--- link_lizard/settings.py
+++ link_lizard/settings.py
@@ -130,4 +130,4 @@ STATICFILES_DIRS = [
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Activate Django-Heroku.
-django_heroku.settings(locals())
+django_heroku.settings(locals(), test_runner=False)
