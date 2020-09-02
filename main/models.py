c003cc9ebeaa64827daad9a504845273d705d110
Patrick Christ <patrick.christ.91@web.de>
2020-09-02 11:17:30 +0200

    Add regex validation

--- main/models.py
+++ main/models.py
@@ -1,11 +1,14 @@
-from django.core.validators import URLValidator
+from django.core.validators import URLValidator, RegexValidator
 from django.db import models
 from django.utils import timezone
 
 
 class URLMapping(models.Model):
     original_url = models.TextField(blank=False, validators=[URLValidator()])
-    generated_hash = models.TextField(blank=False, unique=True, db_index=True)
+    generated_hash = models.TextField(
+        blank=False, unique=True, db_index=True,
+        validators=[RegexValidator(regex='^[a-zA-Z0-9_]+$',
+                                   message="Hash should be a combination of letters and numbers.")])
     created = models.DateTimeField(editable=False)
 
     # pylint: disable=signature-differs
