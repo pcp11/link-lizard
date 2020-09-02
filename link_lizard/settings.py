2c7690f6701bf1dd0a7b018acf3dd4da1c5d2c4e
Patrick Christ <patrick.christ.91@web.de>
2020-09-02 11:27:02 +0200

    Switch to postgres db

--- link_lizard/settings.py
+++ link_lizard/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
 if HEROKU:
     DATABASES = {
         'default': dj_database_url.config(
-            default=os.environ['MONGO_URL']
+            conn_max_age=600, default=os.environ['DATABASE_URL']
         )
     }
 else:
